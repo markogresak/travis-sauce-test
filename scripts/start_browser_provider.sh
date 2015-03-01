08b0289ce28ded92a452ab5ca1d99f57d06b333c
Marko Grešak <marko@gresak.io>
2015-03-01 02:43:14 +0100

    use different start browser provider script

--- scripts/start_browser_provider.sh
+++ scripts/start_browser_provider.sh
@@ -1,40 +1,31 @@
-CONNECT_URL="https://saucelabs.com/downloads/sc-4.3.6-linux.tar.gz"
-CONNECT_DIR="/tmp/sauce-connect-$RANDOM"
-CONNECT_DOWNLOAD="sc-4.3.6-linux.tar.gz"
+#!/bin/bash
 
-CONNECT_LOG="$LOGS_DIR/sauce-connect"
-CONNECT_STDOUT="$LOGS_DIR/sauce-connect.stdout"
-CONNECT_STDERR="$LOGS_DIR/sauce-connect.stderr"
+# Setup and start Sauce Connect for your TravisCI build
+# This script requires your .travis.yml to include the following two private env variables:
+# SAUCE_USERNAME
+# SAUCE_ACCESS_KEY
+# Follow the steps at https://saucelabs.com/opensource/travis to set that up.
+#
+# Curl and run this script as part of your .travis.yml before_script section:
+# before_script:
+#   - curl https://gist.github.com/santiycr/5139565/raw/sauce_connect_setup.sh | bash
+
+CONNECT_URL="http://saucelabs.com/downloads/Sauce-Connect-latest.zip"
+CONNECT_DIR="/tmp/sauce-connect-$RANDOM"
+CONNECT_DOWNLOAD="Sauce_Connect.zip"
+READY_FILE="connect-ready-$RANDOM"
 
 # Get Connect and start it
 mkdir -p $CONNECT_DIR
 cd $CONNECT_DIR
-curl $CONNECT_URL -o $CONNECT_DOWNLOAD 2> /dev/null 1> /dev/null
-mkdir sauce-connect
-tar --extract --file=$CONNECT_DOWNLOAD --strip-components=1 --directory=sauce-connect > /dev/null
+curl $CONNECT_URL > $CONNECT_DOWNLOAD
+unzip $CONNECT_DOWNLOAD
 rm $CONNECT_DOWNLOAD
+java -jar Sauce-Connect.jar --readyfile $READY_FILE \
+    --tunnel-identifier $TRAVIS_JOB_NUMBER \
+    $SAUCE_USERNAME $SAUCE_ACCESS_KEY &
 
-SAUCE_ACCESS_KEY=`echo $SAUCE_ACCESS_KEY | rev`
-
-ARGS=""
-
-# Set tunnel-id only on Travis, to make local testing easier.
-if [ ! -z "$TRAVIS_JOB_NUMBER" ]; then
-  ARGS="$ARGS --tunnel-identifier $TRAVIS_JOB_NUMBER"
-fi
-if [ ! -z "$BROWSER_PROVIDER_READY_FILE" ]; then
-  ARGS="$ARGS --readyfile $BROWSER_PROVIDER_READY_FILE"
-fi
-
-
-echo "Starting Sauce Connect in the background, logging into:"
-echo "  $CONNECT_LOG"
-echo "  $CONNECT_STDOUT"
-echo "  $CONNECT_STDERR"
-sauce-connect/bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY $ARGS -v \
-  --logfile $CONNECT_LOG 2> $CONNECT_STDERR 1> $CONNECT_STDOUT &
-
-# wait for browser provider
-while [ ! -f $BROWSER_PROVIDER_READY_FILE ]; do
+# Wait for Connect to be ready before exiting
+while [ ! -f $READY_FILE ]; do
   sleep .5
 done
